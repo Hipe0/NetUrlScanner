b79496a174ffbd689ad0c1afb615ecc31cc79147
Counting <counting1208@gmail.com>
2026-06-22 18:11:41 +0700

    Cấu hình đính kèm database local vào thư mục App_Data và bỏ chặn file mdf/ldf trong gitignore

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ using NetURLScanner.Options;
 using NetURLScanner.Services;
 using System.Reflection;
 
+AppDomain.CurrentDomain.SetData("DataDirectory", System.IO.Directory.GetCurrentDirectory());
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<AdminSeedOptions>(
